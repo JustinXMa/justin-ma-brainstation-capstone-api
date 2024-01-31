01ed5df7d9444cb2aa24e1d4bbaf99517c741670
Justin <justin.ma@live.ca>
2024-01-31 02:22:49 -0500

    added sample data to tables

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const path = require('path');
 
 const PORT = process.env.PORT;
 
@@ -10,6 +11,9 @@ const usersRoutes = require("./routes/users-routes");
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/users', usersRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
